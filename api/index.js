7fb60aa325eb3548607a7be57faca73444d6d216
Rahil666 <rahilramesh666@gmail.com>
2026-02-09 13:10:37 +0530

    fi

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const sequelize = require('./config/database');
-const propertyRoutes = require('./routes/propertyRoutes');
-const inquiryRoutes = require('./routes/inquiryRoutes');
+const sequelize = require('../config/database');
+const propertyRoutes = require('../routes/propertyRoutes');
+const inquiryRoutes = require('../routes/inquiryRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
